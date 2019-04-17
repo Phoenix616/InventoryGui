d21b6e8f54f999fb03464922192d185400bfe780
Phoenix616 <mail@moep.tv>
2019-04-17 17:47:43 +0100

    Improve item text set method logic

--- src/main/java/de/themoep/inventorygui/InventoryGui.java
+++ src/main/java/de/themoep/inventorygui/InventoryGui.java
@@ -903,20 +903,18 @@ public class InventoryGui implements Listener {
      * @param text  The text lines to set
      */
     public void setItemText(ItemStack item, String... text) {
-        if (item != null) {
+        if (item != null && text != null && text.length > 0) {
             ItemMeta meta = item.getItemMeta();
             if (meta != null) {
-                if (text != null && text.length > 0) {
-                    String combined = replaceVars(Arrays.stream(text)
-                            .filter(s -> !s.isEmpty())
-                            .collect(Collectors.joining("\n")));
-                    String[] lines = combined.split("\n");
-                    meta.setDisplayName(lines[0]);
-                    if (lines.length > 1) {
-                        meta.setLore(Arrays.asList(Arrays.copyOfRange(lines, 1, lines.length)));
-                    } else {
-                        meta.setLore(null);
-                    }
+                String combined = replaceVars(Arrays.stream(text)
+                        .filter(s -> !s.isEmpty())
+                        .collect(Collectors.joining("\n")));
+                String[] lines = combined.split("\n");
+                meta.setDisplayName(lines[0]);
+                if (lines.length > 1) {
+                    meta.setLore(Arrays.asList(Arrays.copyOfRange(lines, 1, lines.length)));
+                } else {
+                    meta.setLore(null);
                 }
                 item.setItemMeta(meta);
             }
